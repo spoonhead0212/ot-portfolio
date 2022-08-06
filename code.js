1c9b73f736d0597939d115c7fcf663bf2e7b3684
spoonhead0212 <spoonheadmycon@gmail.com>
2022-08-06 01:12:12 +0100

    we lit

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".hamburger-menu").click(function(){
-        $(".navvy").toggle(1000);
+        $(".navvy").toggle(500);
     });
 });
 //.hide() is the syntax used to hide element
