ebe88acbf0878d76c7d3d832f51b060542175478
spoonhead0212 <spoonheadmycon@gmail.com>
2022-10-21 22:40:31 +0100

    some new update

--- code.js
+++ code.js
@@ -2,12 +2,6 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
-// $(document).ready(function(){
-//       $(".hamburger-menu").click(function(){
-//         $(".navvy").slideToggle(500);
-//     });
-// });
-
 
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 let menuDiv = document.querySelector(".navvy");
