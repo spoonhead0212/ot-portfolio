114ac5d5a2faa712ec1bf7f463f5ff863fb7e3dd
spoonhead0212 <spoonheadmycon@gmail.com>
2022-10-22 11:20:56 +0100

    menu adjust

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
-function myFunction(x) {
+ function myFunction(x) {
   x.classList.toggle("change");
-}
+ }
 
 
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 let menuDiv = document.querySelector(".navvy");
 hamburgerMenu.addEventListener("click", () => {
+  
   menuDiv.classList.toggle("hamburger-show")
 })
