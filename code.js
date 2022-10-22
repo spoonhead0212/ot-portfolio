a0f99546c1fffac902c1edb64fc3c390da93fd3f
spoonhead0212 <spoonheadmycon@gmail.com>
2022-10-22 12:58:54 +0100

    .

--- code.js
+++ code.js
@@ -1,11 +1,24 @@
+//  change menu icon class
  function myFunction(x) {
   x.classList.toggle("change");
  }
 
+// navigation appearance on mobile
 
 let hamburgerMenu = document.querySelector(".hamburger-menu");
-let menuDiv = document.querySelector(".navvy");
 hamburgerMenu.addEventListener("click", () => {
-  
+  let menuDiv = document.querySelector(".navvy");
   menuDiv.classList.toggle("hamburger-show")
 })
+
+// // 
+// window.addEventListener("scroll", () => {
+//   let menuNav = document.querySelector(".navigations")
+//    let currentScroll = window.pageXOffset;
+//   //  currentScroll tracks the page scroll level
+//   if (currentScroll >= 300) {
+//     menuNav.classList.add("hide-show-nav")
+//   } else {
+//     menuNav.classList.remove("hide-show-nav")
+//   }
+// })
